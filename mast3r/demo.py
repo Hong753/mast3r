9974f47b834da74349610e3744550d79abc67dd0
Hong753 <hongjung753@gmail.com>
2026-04-21 14:23:47 +0900

    Updated demo.py

--- mast3r/demo.py
+++ mast3r/demo.py
@@ -242,7 +242,7 @@ def set_scenegraph_options(inputfiles, win_cyclic, refid, scenegraph_type):
         winsize = gradio.Slider(label="Retrieval: Num. key images", value=min(20, num_files),
                                 minimum=0, maximum=num_files, step=1, visible=True)
         win_cyclic = gradio.Checkbox(visible=False)
-        refid = gradio.Slider(label="Retrieval: Num neighbors", value=min(num_files - 1, 10), minimum=1,
+        refid = gradio.Slider(label="Retrieval: Num neighbors", value=min(num_files - 1, 5), minimum=1,
                               maximum=num_files - 1, step=1, visible=True)
 
     elif scenegraph_type == "oneref":
